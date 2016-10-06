03a885e8736d56b04b7e15cc8c13c8605ceb74f5
Ben Gotow <bengotow@gmail.com>
2016-10-05 23:16:27 -0700

    Working notes example

--- example/renderer.js
+++ example/renderer.js
@@ -1,16 +1,26 @@
-import {DatabaseObjectRegistry, DatabaseStore, Model, Attributes, QuerySubscription} from 'electron-coresqlite';
+import {DatabaseObjectRegistry, DatabaseStore, Model, Attributes} from 'electron-coresqlite';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-class Bla extends Model {
-  static attributes = {
+class Note extends Model {
+  static attributes = Object.assign(Model.attributes, {
     name: Attributes.String({
       modelKey: 'name',
       jsonKey: 'name',
       queryable: true,
     }),
-  };
+    content: Attributes.String({
+      modelKey: 'content',
+      jsonKey: 'content',
+    }),
+    createdAt: Attributes.DateTime({
+      modelKey: 'createdAt',
+      jsonKey: 'createdAt',
+      queryable: true,
+    }),
+  });
 }
-
-DatabaseObjectRegistry.register(Bla.constructor.name, () => Bla)
+DatabaseObjectRegistry.register(Note.constructor.name, () => Note)
 
 window.dbStore = new DatabaseStore({
   primary: true,
@@ -20,11 +30,195 @@ window.dbStore = new DatabaseStore({
   logQueryPlans: false,
 });
 
-const observable = window.dbStore.findAll(Bla).where({name: 'Foo'}).observe()
-observable.subscribe((objects) => {
-  console.log(objects);
-});
+class SidebarItem extends React.Component {
+  static propTypes = {
+    item: React.PropTypes.object,
+    isSelected: React.PropTypes.bool,
+    onSelected: React.PropTypes.func,
+  };
 
-window.dbStore.inTransaction((t) => {
-  t.persistModel(new Bla({name: "Foo"}));
+  constructor(props) {
+    super(props);
+    this.state = {editing: null};
+  }
+
+  _onDoubleClick = () => {
+    const el = ReactDOM.findDOMNode(this);
+    el.focus();
+    const range = document.createRange();
+    range.selectNodeContents(el);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    this.setState({editing: true});
+  }
+
+  _onKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      event.target.blur();
+    }
+  }
+
+  _onBlur = () => {
+    const item = this.props.item;
+    item.name = ReactDOM.findDOMNode(this).innerText;
+    window.dbStore.inTransaction((t) => {
+      t.persistModel(item);
+    });
+    this.setState({editing: null});
+  }
+
+  render() {
+    const {isSelected, onSelected, item} = this.props;
+    const {editing} = this.state;
+
+    const className = `item ${isSelected ? " selected" : ""}`;
+
+    return (
+      <div
+        className={className}
+        onDoubleClick={this._onDoubleClick}
+        onBlur={this._onBlur}
+        onKeyDown={this._onKeyDown}
+        onClick={onSelected}
+        contentEditable={editing}
+        dangerouslySetInnerHTML={{__html: item.name}}
+      />
+    )
+  }
+}
+
+class Sidebar extends React.Component {
+  static propTypes = {
+    items: React.PropTypes.array,
+    selectedItem: React.PropTypes.object,
+    onSelect: React.PropTypes.func,
+  };
+
+  render() {
+    const {items, onSelect, selectedItem} = this.props;
+
+    return (
+      <div className="sidebar">
+      {
+        items.map((item) => {
+          return (
+            <SidebarItem
+              key={item.id}
+              item={item}
+              isSelected={selectedItem === item}
+              onSelected={() => onSelect(item)}
+            />
+          );
+        })
+      }
+      </div>
+    )
+  }
+}
+
+class Detail extends React.Component {
+  static propTypes = {
+    item: React.PropTypes.object,
+  };
+
+  _onBlur = () => {
+    const {item} = this.props;
+    item.content = this.refs.content.innerHTML;
+    item.name = this.refs.name.innerText;
+
+    window.dbStore.inTransaction((t) => {
+      t.persistModel(item);
+    });
+  }
+
+  render() {
+    if (!this.props.item) {
+      return (
+        <div className="detail">
+          Please select an item
+        </div>
+      );
+    }
+
+    const {name, content} = this.props.item;
+    return (
+      <div className="detail">
+        <h2
+          ref="name"
+          contentEditable
+          onBlur={this._onBlur}
+          dangerouslySetInnerHTML={{__html: name}}
+        />
+        <div
+          ref="content"
+          className="content"
+          contentEditable
+          dangerouslySetInnerHTML={{__html: content}}
+          onBlur={this._onBlur}
+        />
+      </div>
+    );
+  }
+}
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      selectedItem: null,
+    };
+  }
+
+  componentDidMount() {
+    const query = window.dbStore.findAll(Note).order(Note.attributes.createdAt.descending());
+    this._unsubscribe = query.observe().subscribe((items) => {
+      this.setState({items});
+    });
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _onSelectItem = (item) => {
+    this.setState({selectedItem: item});
+  }
+
+  _onCreateItem = () => {
+    const item = new Note({
+      name: 'Untitled',
+      content: 'Write your note here!',
+      createdAt: new Date(),
+    });
+    window.dbStore.inTransaction((t) => {
+      return t.persistModel(item);
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Sidebar
+          items={this.state.items}
+          selectedItem={this.state.selectedItem}
+          onSelect={this._onSelectItem}
+        >
+        </Sidebar>
+        <Detail item={this.state.selectedItem} />
+        <div className="floating">
+          <button onClick={this._onCreateItem}>
+            ➕
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('container');
+  ReactDOM.render(<Container />, container);
 });
