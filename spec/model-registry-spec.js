5ce14a28f5a880581d2d1ea2220b51070aabf351
Ben Gotow <bengotow@gmail.com>
2016-10-25 23:11:13 -0700

    fix(registry): Updated interface and fixed specs

--- spec/model-registry-spec.js
+++ spec/model-registry-spec.js
@@ -12,30 +12,54 @@ class GoodTest extends Model {
   });
 }
 
+class BetterTest extends Model {
+  static attributes = Object.assign({}, Model.attributes, {
+    "bar": Attributes.String({
+      modelKey: 'bar',
+      jsonKey: 'bar',
+    }),
+  });
+}
+
+class RandomClass {
+
+}
+
 describe('ModelRegistry', function ModelRegistrySpecs() {
   beforeEach(() => {
     this.registry = new ModelRegistry();
+    this.registry.registerDeferred({name: "GoodTest", resolver: () => GoodTest});
   });
 
-  it("can register constructors", () => {
-    const testFn = () => GoodTest;
-    expect(() => this.registry.register("GoodTest", testFn)).not.toThrow();
-    expect(this.registry.get("GoodTest")).toBe(GoodTest);
+  describe("registerDeferred", () => {
+    it("can register constructors", () => {
+      const testFn = () => BetterTest;
+      this.registry.registerDeferred({name: "BetterTest", resolver: testFn});
+      expect(this.registry.get("BetterTest")).toBe(BetterTest);
+    });
   });
 
-  it("Tests if a constructor is in the registry", () => {
-    this.registry.register("GoodTest", () => GoodTest);
-    expect(this.registry.isInRegistry("GoodTest")).toBe(true);
+  describe("has", () => {
+    it("tests if a constructor is in the registry", () => {
+      expect(this.registry.has("GoodTest")).toEqual(true);
+      expect(this.registry.has("BadTest")).toEqual(false);
+    });
   });
 
-  it("deserializes the objects for a constructor", () => {
-    this.registry.register("GoodTest", () => GoodTest);
-    const obj = this.registry.deserialize("GoodTest", {foo: "bar"});
-    expect(obj instanceof GoodTest).toBe(true);
-    expect(obj.foo).toBe("bar");
-  });
+  describe("deserialize", () => {
+    it("deserializes the objects for a constructor", () => {
+      const obj = this.registry.deserialize("GoodTest", {foo: "bar"});
+      expect(obj instanceof GoodTest).toBe(true);
+      expect(obj.foo).toBe("bar");
+    });
 
-  it("throws an error if the object can't be deserialized", () =>
-    expect(() => this.registry.deserialize("GoodTest", {foo: "bar"})).toThrow()
-  );
+    it("throws an error if the object can't be deserialized", () =>
+      expect(() => this.registry.deserialize("BadTest", {foo: "bar"})).toThrow()
+    );
+
+    it("throws if the registered constructor was not a model subclass", () => {
+      this.registry.registerDeferred({name: "RandomClass", resolver: () => RandomClass});
+      expect(() => this.registry.deserialize("RandomClass", {foo: "bar"})).toThrow();
+    });
+  });
 });
