bc0e38a16e61079e203f0474fa67a945278d66a4
Ben Gotow <bengotow@gmail.com>
2016-11-25 02:10:14 -0800

    fix(package): Update post-install script to find node-gyp

--- post-install.js
+++ post-install.js
@@ -54,20 +54,28 @@ if (!targetElectronVersion) {
 }
 
 // prepare other params
-
-var nodeGypPath = '"' + require('path').resolve(__dirname, 'node_modules', '.bin', 'node-gyp') + '"';
 var targetArch = require('os').arch();
 var targetPlatform = process.platform;
-
 if (targetPlatform == "win32") {
   var targetArch = "ia32"
 }
 
-var pathToSqlite = ['../better-sqlite3', './node_modules/better-sqlite3'].find((p) => fs.existsSync(p));
+var pathToNodeGyp = [
+  path.resolve(__dirname, 'node_modules', '.bin', 'node-gyp'),
+  path.resolve(__dirname, '..', '.bin', 'node-gyp'),
+].find((p) => fs.existsSync(p));
+if (!pathToNodeGyp) {
+  throw new Error("Couldn't find node-gyp");
+}
+
+var pathToSqlite = [
+  '../better-sqlite3',
+  './node_modules/better-sqlite3'
+].find((p) => fs.existsSync(p));
 if (!pathToSqlite) {
   throw new Error("Couldn't find better-sqlite3");
 }
 
-var cmd = "cd " + pathToSqlite + " && "+nodeGypPath+" configure rebuild --msvs_version=2013 --target="+targetElectronVersion+" --arch="+targetArch+" --target_platform="+targetPlatform+" --dist-url=https://atom.io/download/electron";
+var cmd = "cd " + pathToSqlite + " && \""+pathToNodeGyp+"\" configure rebuild --msvs_version=2013 --target="+targetElectronVersion+" --arch="+targetArch+" --target_platform="+targetPlatform+" --dist-url=https://atom.io/download/electron";
 console.log(cmd);
 execSync(cmd);
